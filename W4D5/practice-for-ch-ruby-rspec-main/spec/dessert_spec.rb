593c4aecce8683975b3631b24d0ee5c6a2c9bdb9
yuris <yls4@cornell.edu>
2023-09-14 20:37:40 -0400

    added  dessert spec, completed dessert spec

--- W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
+++ W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
@@ -6,24 +6,25 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
-  subject(:dessert) {Dessert.new("pastry", 10, :chef)}
+  let(:chef) { double("chef", name: "Chef Boyardi") }
+  
+  subject(:dessert) {Dessert.new("pastry", 10, chef)}
 
   describe "#initialize" do
     it "sets a type" do 
-      expect(dessert.type).to_eq(pastry)
+      expect(dessert.type).to eq("pastry")
     end
 
     it "sets a quantity" do
-      expect(dessert.quantity).to_eq(10)
+      expect(dessert.quantity).to eq(10)
     end
 
     it "starts ingredients as an empty array" do
-      expect(dessert.ingredients).to_eq([])
+      expect(dessert.ingredients).to eq([])
     end
 
     it "raises an argument error when given a non-integer quantity" do
-      expect {Dessert.new("pastry",-6, :chef}.to raise_error(ArgumentError)
+      expect {Dessert.new("pastry", 1.2, :chef)}.to raise_error(ArgumentError)
     end
   end
 
@@ -50,21 +51,24 @@ describe Dessert do
   end
 
   describe "#eat" do
+    before(:each) do 
+      dessert.eat(10)
+    end
     it "subtracts an amount from the quantity" do 
-      before(:each) do 
-        dessert.eat(10)
-      end
       expect(dessert.quantity).to eq(0)
     end
 
     it "raises an error if the amount is greater than the quantity" do 
-      expect(dessert.eat(11)).to raise_error(ArgumentError)
+      expect{dessert.eat(11)}.to raise_error(RuntimeError)
     end
   end
 
   describe "#serve" do
+    let (:chef_title) {dessert.serve.split(" ")[0]}
     it "contains the titleized version of the chef's name" do 
-      expect(chef.titleize
+      allow(chef).to receive(:titleize).and_return("Chef Boyardi")
+      expect(dessert.serve).to eq("Chef Boyardi has made 10 pastries!")
+    end
   end
 
   describe "#make_more" do
