74f31bc917a006c17de9b9da9f521a12f821f793
yuris <yls4@cornell.edu>
2023-09-26 10:55:13 -0400

    commit rails project

--- rails_project/app/models/house.rb
+++ rails_project/app/models/house.rb
@@ -2,7 +2,7 @@ class House < ApplicationRecord
     validates :address, presence: true
 
     has_many( 
-        :people,
+        :residents,
         class_name: 'Person',
         foreign_key: :house_id,
         primary_key: :id
