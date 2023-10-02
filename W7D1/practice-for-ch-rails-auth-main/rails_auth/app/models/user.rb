4ec6aeacd4840a257528ec4e291987559d5311db
yuris <yls4@cornell.edu>
2023-10-02 03:18:14 -0400

    completed initial validations and find_by_credentials

---
+++ W7D1/practice-for-ch-rails-auth-main/rails_auth/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: {message: 'Password missing'}
+    validates :password, length: {minimum: 6, allow_nil: true} 
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user 
+            return use if user.is_password?(password)
+        else
+            return nil
+        end
+    end
+
+    def generate_unique_session_token
+
+    end
+
+    def password=(arg)
+    
+    end
+
+    def is_password?(arg)
+
+    end
+
+    def reset_session_token!
+
+    end
+
+    def ensure_session_token
+
+    end
+
+
+end
