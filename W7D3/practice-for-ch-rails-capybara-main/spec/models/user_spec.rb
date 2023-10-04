f35037be6d8c2f05b4c975e0aa2081a549d86298
yuris <yls4@cornell.edu>
2023-10-04 01:50:47 -0400

    rails model tests complete

---
+++ W7D3/practice-for-ch-rails-capybara-main/spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it {should validate_presence_of(:email)}
+  it {should validate_presence_of(:password_digest)}
+  it {should validate_length_of(:password).is_at_least(6)}
+
+  describe '#is_password?' do
+    it 'should return true if the given password is the same as the user password' do 
+      expect(invalid_password).to_eq(false)
+    end
+  end
+
+end
